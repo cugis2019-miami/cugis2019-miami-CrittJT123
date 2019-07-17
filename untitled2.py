7a323a22e2d61c5227c0c07479c66f1c6cbf87f8
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:23:52 -0400

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:22:54 2019
+
+@author: admin
+"""
+(5/4)*4
+
+
+def plus(a,b):
+    plus = a + b
+    print(plus)
+    
+  plus(100,100000)
+  
+print("box")choclate = 5
+
+print("how many chickens are in the coop?")
+chicken = 3+3
+print("there are", chicken, "in the choop")
+
+print("how many chickens are in the coop?")
+chicken = 3 + chicken
+print("there are", chicken, "in the coop")
+
+print("how many chickens are in the coop?")
+chicken = 3 + 3 + chicken
+print("there are", chicken, "are in the coop?")
+
+D = "dark"
+M = "milky"
+W = "white"
+
+print("in the box, there are",D,M,W, "cholocates")
+
+"D" = dark 
