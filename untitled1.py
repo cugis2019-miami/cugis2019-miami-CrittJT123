c3abfa9b0fa9b593981352795e6b9c3f361098c7
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:22:45 -0400

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:16:02 2019
+
+@author: admin
+"""
+
+"IM LIVING MY BES TLIFE!"
+
+def plus(a,b,c):
+    plus = (1/2)*b*c
+    print(plus)
+
+plus(1/2,2,3)
