2077d5b1e8f7801c08319cf4797188d18756105d
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:23:29 -0400

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 15:48:43 2019
+
+@author: admin
+"""
+
+def cube(x):
+    cube = math.pow(x,1/3)
+    print("The cube root",x,"is",cube)
+    
+cube(27)
+
+'''
+def cube(x):
+    cube = math.pow(x,1/3)
+    print("The cube root",x,"is", cube)
+    
+    cube(3)
+
+
