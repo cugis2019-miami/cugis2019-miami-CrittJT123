e258a315ac3c48d9dd6a6d0fcb28eb2df0cc2c74
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:21:09 -0400

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print("I am learning to code in python today")
+print("chicken noodle soup")
+"53"
+
+
+
+
+5*2
+
+5/2
+
+5-2
+
+5**2
+
+8/9*3
+
+print("5 divided by 2") 
+5/2
+
+print("5 divided by 2 is", 5/2)
+  
+print("my ramen taste like totel water")
+
+"the snack that smiles back, GOLDFISH!"
+
+#interesting Ma'ma
+
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)
+     
+print("1/2*base*height")
+
+def triangle(b,h):
+    triangle = (1/2)*b*h
+    print(triangle)
+             
+triangle(23,23)    
+
+"Today is gonna be a great day!"
