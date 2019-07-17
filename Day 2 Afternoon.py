67daf07b9da113e48c6bc72817fc5dc1fc314abf
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:21:27 -0400

    Add files via upload

---
+++ Day 2 Afternoon.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:22:54 2019
+
+@author: admin
+"""
+
+'''
+(5/4)*4
+
+
+def plus(a,b):
+    plus = a + b
+    print(plus)
+    
+  plus(100,100000)
+  
+print("box")choclate = 5
+
+print("how many chickens are in the coop?")
+chicken = 3+3
+print("there are", chicken, "in the choop")
+
+print("how many chickens are in the coop?")
+chicken = 3 + chicken
+print("there are", chicken, "in the coop")
+
+print("how many chickens are in the coop?")
+chicken = 3 + 3 + chicken
+print("there are", chicken, "are in the coop?")
+
+D = "dark"
+M = "milky"
+W = "white"
+
+print("in the box, there are",D,M,W, "cholocat
+      
+      
+      d = "dark"
+ print("white, black, orange, blue, yellow",d,"and bright")
+
+T = "The Crusty Crab"
+
+print("Spongebob works at",T,)
+
+a = "(girl answers):Layla"
+t = "Hello! My name is Masafa, what's yours?"
+
+print(t,a)
+
+
+a = ", what's yours?"
+
+print("Hello my name is Jaden Sanchaz",a)
+
+def Name(Jalla):
+    Name = Jalla
+    Print(Name)
+    
+print("Hello, what's your name?", Name ,"My name is Jalla, and it is nice to meet you")
+
+
+def Hello(name):
+    print(name)
+
+Hello("Susan")
+
+"It's the best day EVER!, the best day EVER!"
+
+t = "Power-Rangers Samurai"
+y = "ah-hem, ah-hem, ah-ah-aha-ah, Go go Power-Rangers"
+z = "Don't you ever STOP!, Go go Power-Rangers, till the very end"
+a = "Rangers together, samurai forever!"
+
+print(t,y,z,a)
+
+kim = "5"
+
+"How old is your daughter, Sir?  She is", kim, "years old"
+'''
+import math
+
+dir(math)
+
+math.pi
+
+math.factorial(0)
+
+dir(math)
+
+math.inf
+
+math.e
+
+math.remainder
+
+dir(math)
+
+math.pow
+
+10/7 
+math.pow(2,4)
+
+math.pow(6,1)
+
+math.pow(6,8/2)
+
+math.pow(0,0)
+
+math.remainder(5,3)
+
+math.remainder(7,8/3)
+
+dir(math)
+
+ 
+
+cubicarea(5,a)
+print(" The cubic root of the )
+
+def cubicroot(vaule):
+    cubicroot = (vaule)**(1/3)
+    print(cubicroot)
+    
+    cubicroot(3)
+    math.pow(3,5)
+math.pow(243.0,1/3)
+
+    
+print("The cubi croot of the" vaule, "is" exponent,)
+
+def cubicroot(value):
+    cubicroot =math.pow(value,1/3)
+    print("The cubicroot of",a, "is",cubicroot)
+
+    
+cubicroot(3)
+
+dir(math)    
+
+print("6 times 4 is",6*4,)
+
+def pll(x):
+    x
+    print(cubricroot)
+    
+
+userValue=27
+
+cubicroot(userValue)
+    
+
+    
