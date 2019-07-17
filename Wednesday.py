f6b1b0dc63d7aecdf03cce5d75f38cabacc2c4e7
CrittJT123 <52932045+CrittJT123@users.noreply.github.com>
2019-07-17 16:22:03 -0400

    Add files via upload

---
+++ Wednesday.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 09:59:04 2019
+
+@author: admin
+"""
+
+#Beginner coding Lesson #3
+M = "5 milk cholocates,"
+d = "3 dark Cholocates,"
+w = "and 8 white cholocates."
+
+print("In the box, there are",M,d,w)
+
+print("there")
+ t = "armstrong"
+ g = "was"
+ h = "here"
+ 
+ print("Who was here today?", t,g,h)
+ 
+print("there are")
+m = "five nations"
+print("there are none!")
+
+print("there are",m,"there are none")
+
+
+chocolate1= {"milk":5}
+
+chocolate2= {"dark":3}
+
+choloate3= {"white":8}
+
+
+
+print("the nubmer of chocolates in the box are", choloatebox)
+
+
+print("Theses are the pople listed within the profile list",People)
+
+S = {"Steve":32}
+L = {"Lia":28}
+V = {"Vin":45}
+K = {"Katie":38}
+
+print("This profile list both the people and their ages who appiled
+      for this job,",S,L,V,K)
+    
+
+choloatebox = {"milk":5,"dark":3,"white":8}
+print("The number of dark cholocates in cholocate box is", choloatebox["dark"])
+
+
+
+print("Lia's age and gender is",People["Lia"])
+'''
+#Age and Gender
+Gender = {"Lia":"Female","Steve":"Male","Vin":"Male","Katie":"Female"}
+Age = {"Steve ":32,"Lia":28,"Vin":45,"Katie":38}
+print("The age and gender of Vin is",Age["Vin"] ,"and he is a",Gender["Vin"])
+print("the age and gender of Lia is",Age["Lia"] , "and she is a",Gender["Vin"])
